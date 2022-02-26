eceab4c321af6c1813395208f0cfd35abe44a92d
laskor <al.moksud@gmail.com>
2022-02-26 00:01:18 -0500

    case convert done

--- case-convert.js
+++ case-convert.js
@@ -1,1 +1,9 @@
 const anthem = 'Shunar Bangla Ami tumay Valobashi';
+const search = 'valo';
+
+const userInput = 'bLackpink';
+const savedUserName = 'blackpink';
+
+if(userInput.toLowerCase() == savedUserName.toLowerCase()){
+    console.log('user exits');
+}
