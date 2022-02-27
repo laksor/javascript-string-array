e594ac48001522e2cb0d67afea4a732f7624639a
laskor <al.moksud@gmail.com>
2022-02-26 21:42:46 -0500

    concat , join

---
+++ concat-join.js
@@ -0,0 +1,16 @@
+// concat
+
+const first = 'My';
+const second = 'City';
+
+const result = first.concat(second);
+
+console.log(result);
+
+// join
+
+const words = ['a', 'b', 'c', 'd'];
+const joined = words.join(', ');
+// const joined = words.join(' ');
+
+console.log(joined);
