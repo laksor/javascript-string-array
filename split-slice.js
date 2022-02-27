99ceeac3e9dcbd7b3c1ab98621b7b7e5fd872ed7
laskor <al.moksud@gmail.com>
2022-02-26 21:51:55 -0500

    split, slice done

---
+++ split-slice.js
@@ -0,0 +1,12 @@
+// split
+const anthem = 'Amar sonar bangla';
+const words = anthem.split(' ');
+const wordsA = anthem.split('a');
+
+console.log(words);
+console.log(wordsA);
+
+// slice
+
+const smallslice = anthem.slice(11,17);
+console.log(smallslice);
